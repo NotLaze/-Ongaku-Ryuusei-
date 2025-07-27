3903683dce6a6cb1287590c6bbbdc976ec0155ec
NotLaze <notlazeyt@gmail.com>
2025-07-27 22:18:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const audio = document.getElementById('audio');
+const playBtn = document.getElementById('playBtn');
+const pauseBtn = document.getElementById('pauseBtn');
+
+playBtn.addEventListener('click', () => audio.play());
+pauseBtn.addEventListener('click', () => audio.pause());
